ffad8fdf6877a5aeddb9a444d331a9f92254f339
smishra11 <msubhasish11@gmail.com>
2021-01-07 03:14:30 +0530

    Added full UI of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Google from './assets/google.svg';
+import Info from './assets/info.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard">
+      <div className="layout">
+        <div className="d-flex justify-content-center">
+          <h1 className="sign_up">Sign Up</h1>
+        </div>
+        <div className="btn_container">
+          <button className="btn btn-primary btn_google">
+            <img
+              src={Google}
+              alt="ggl icon"
+              style={{ paddingBottom: '5px', paddingRight: '10px' }}
+            />
+            Continue with Google
+          </button>
+        </div>
+        <hr className="mt-4 mb-4" />
+        <div className="d-flex justify-content-center">
+          <div className="email_container">
+            <p style={{ margin: '0', fontSize: '13px', color: '#9B9EB0' }}>
+              <strong>Email Address</strong>
+            </p>
+            <p style={{ color: '181229' }}>rajabishek@hotmail.com</p>
+          </div>
+        </div>
+        <div className="info_container ml-2">
+          <div className="d-flex justify-content-center mr-2 ml-2">
+            <img src={Info} alt="info_icon" />
+          </div>
+          <div>
+            <p style={{ margin: '0' }}>
+              <strong>Please check your email</strong>
+            </p>
+            <p style={{ fontSize: '15px' }}>
+              We’ve sent a temporary authorization code to
+              rajabisek@hotmail.com. It may take a few minutes to arrive.
+            </p>
+          </div>
+        </div>
+        <div className="m-2">
+          <input
+            className="form-control login_input"
+            type="text"
+            placeholder="Please paste authorization code"
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className="btn login_btn ">
+            <strong>Log In</strong>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
